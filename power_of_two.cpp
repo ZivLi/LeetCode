db616465f8b85f296a31dd0f047797fb4ac1fd38
ZivLi <zivli23@163.com>
2017-07-04 14:49:18 +0800

    rename the file.

--- power_of_two.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-class Solution {
-public:
-    bool isPowerOfTwo(int n) {
-        return n > 0 and !(n&(n-1));
-    }
-};
--- word_frequency.sh
+++
@@ -1,2 +0,0 @@
-awk -F' ' '{for(i=1;i<=NF;i=i+1){print $i}}' ./words.txt |
-sort|uniq -c|sort -nr|awk -F' ' '{printf("%s %s\n",$2,$1)}'
