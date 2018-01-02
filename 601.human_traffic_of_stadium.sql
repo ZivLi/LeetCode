00065651f0885bd91ce1e1584221be58fefe3597
ZivLi <zivli23@163.com>
2018-01-02 11:46:57 +0800

    format sql

--- 601.human_traffic_of_stadium.sql
+++ 601.human_traffic_of_stadium.sql
@@ -1,8 +1,10 @@
 select s1.*
 from stadium s1, stadium s2, stadium s3
 where (
-  (s1.id+1=s2.id and s1.id+2=s3.id) or 
-  (s1.id-1=s2.id and s1.id+1=s3.id) or 
+  (s1.id+1=s2.id and s1.id+2=s3.id)
+  or 
+  (s1.id-1=s2.id and s1.id+1=s3.id)
+  or 
   (s1.id-2=s2.id and s1.id-1=s3.id)
 )
 and s1.people >= 100
