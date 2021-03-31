caa8849d03b02b366a10d9eb17263bb3d0cded22
ZivLi <zivli23@163.com>
2021-03-31 16:48:13 +0800

    148

---
+++ 148.排序链表.py
@@ -0,0 +1,31 @@
+#
+# @lc app=leetcode.cn id=148 lang=python3
+#
+# [148] 排序链表
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def sortList(self, head: ListNode) -> ListNode:
+        if not head or not head.next: return head
+        slow, fast = head, head.next
+        while fast and fast.next:
+            fast, slow = fast.next.next, slow.next
+        mid, slow.next = slow.next, None
+        left, right = self.sortList(head), self.sortList(mid)
+        h = res =  ListNode(0)
+        while left and right:
+            if left.val < right.val:
+                h.next, left = left, left.next
+            else:
+                h.next, right = right, right.next
+            h = h.next
+        h.next = left if left else right
+        return res.next
+# @lc code=end
+
