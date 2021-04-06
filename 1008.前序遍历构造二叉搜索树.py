ecf6c661c3eeb1b5ffcbfb74cacd102ed569c39c
ZivLi <zivli23@163.com>
2021-04-06 18:39:34 +0800

    引入中间变量

---
+++ 1008.前序遍历构造二叉搜索树.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode.cn id=1008 lang=python3
+#
+# [1008] 前序遍历构造二叉搜索树
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def bstFromPreorder(self, preorder: List[int]) -> TreeNode:
+# @lc code=end
+
---
+++ 783.二叉搜索树节点最小距离.py
@@ -0,0 +1,28 @@
+#
+# @lc app=leetcode.cn id=783 lang=python3
+#
+# [783] 二叉搜索树节点最小距离
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def minDiffInBST(self, root: TreeNode) -> int:
+        ans = float('-inf')
+        prev = float('-inf')
+        def dfs(node):
+            if node:
+                dfs(node.left)
+                self.ans = min(self.ans, node.val - self.prev)
+                self.prev = node.val
+                dfs(node.right)
+        dfs(root)
+        return self.ans
+
+# @lc code=end
+
