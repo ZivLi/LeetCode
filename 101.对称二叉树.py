5ce00dda978581682ab6a1a5191ca186e6785e14
ZivLi <zivli23@163.com>
2021-04-08 17:07:22 +0800

    101

--- 101.对称二叉树.py
+++ 101.对称二叉树.py
@@ -13,5 +13,14 @@
 #         self.right = right
 class Solution:
     def isSymmetric(self, root: TreeNode) -> bool:
+        def is_mirror(node_l, node_r):
+            if not node_l and not node_r:
+                return True
+            if not node_l or not node_r:
+                return False
+            if node_l.val == node_r.val:
+                return is_mirror(node_l.left, node_r.right) and is_mirror(node_l.right, node_r.left)
+            return False
+        return is_mirror(root, root)
 # @lc code=end
 
