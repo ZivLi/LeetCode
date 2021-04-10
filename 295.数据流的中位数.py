2a9079d0a1ec3673a36645220149e704c0d60fa3
ZivLi <zivli23@163.com>
2021-04-10 19:36:41 +0800

    295,维护一个最小堆和最大堆,中位数左边&右边,维护两边长度保持中位数两边长相等

---
+++ 295.数据流的中位数.py
@@ -0,0 +1,39 @@
+#
+# @lc app=leetcode.cn id=295 lang=python3
+#
+# [295] 数据流的中位数
+#
+
+# @lc code=start
+class MedianFinder:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.min_heap = []
+        self.max_heap = []
+
+    def addNum(self, num: int) -> None:
+        if not self.max_heap or num < -self.max_heap[0]:
+            heapq.heappush(self.max_heap, -num)
+        else:
+            heapq.heappush(self.min_heap, num)
+
+        if len(self.max_heap) > len(self.min_heap) + 1:
+            heappush(self.min_heap, -heappop(self.max_heap))
+        elif len(self.min_heap) > len(self.max_heap):
+            heappush(self.max_heap, -heappop(self.min_heap))
+
+    def findMedian(self) -> float:
+        return -self.max_heap[0] if len(self.max_heap) != len(self.min_heap) else \
+            (self.min_heap[0]-self.max_heap[0]) / 2
+
+
+
+# Your MedianFinder object will be instantiated and called as such:
+# obj = MedianFinder()
+# obj.addNum(num)
+# param_2 = obj.findMedian()
+# @lc code=end
+
